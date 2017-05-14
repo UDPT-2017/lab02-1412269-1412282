998c0081d5405e30eb056bb62e1ee2c9ff248d3b
KietHa <hamanhkiet77@gmail.com>
2017-05-14 13:14:29 +0700

    add hbs page engine

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
 
 app.use(express.static('public'));
 app.use('/components', express.static('bower_components'));
-//routing
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+app.engine('hbs', exphbs({}));
+app.set('view engine', 'hbs');
+
+//routes
+app.get('/hbs', function (req, res) {
+  res.render('index', {title: 'HBS Index Page', message: 'Hello HBS Page'});
+});
+app.get('/messages', function (req, res) {
+  res.render('messages', {title: 'HBS Messages Page', message: 'My Messages'});
+});
+app.get('/users', function (req, res) {
+  res.render('users', {title: 'HBS Users Page', message: 'My Users'});
+});
+app.get('/about', function (req, res) {
+  res.render('about', {title: 'HBS About Page', message: 'My About'});
 });
 
 //start server
