ae937ef4e59b9dd3c55e5735632afb024a8ab316
KietHa <hamanhkiet77@gmail.com>
2017-05-15 07:30:18 +0700

    handlebars layout

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 var express = require('express');
 var app = express();
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 
 app.use(express.static('public'));
 app.use('/components', express.static('bower_components'));
 
-app.engine('hbs', exphbs({}));
+app.engine('hbs', exphbs({
+  extname: '.hbs',
+  defaultLayout: 'application'
+}));
 app.set('view engine', 'hbs');
 
 //routes
-app.get('/hbs', function (req, res) {
-  res.render('index', {title: 'HBS Index Page', message: 'Hello HBS Page'});
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Home',
+        message: 'Hello HBS Page',
+    });
 });
-app.get('/messages', function (req, res) {
-  res.render('messages', {title: 'HBS Messages Page', message: 'My Messages'});
+app.get('/messages', function(req, res) {
+    res.render('messages', {
+        title: 'Messages',
+        message: 'My Messages',
+    });
 });
-app.get('/users', function (req, res) {
-  res.render('users', {title: 'HBS Users Page', message: 'My Users'});
+app.get('/users', function(req, res) {
+    res.render('users', {
+        title: 'Users',
+        message: 'My Users',
+    });
 });
-app.get('/about', function (req, res) {
-  res.render('about', {title: 'HBS About Page', message: 'My About'});
+app.get('/about', function(req, res) {
+    res.render('about', {
+        title: 'About',
+        message: 'My About',
+    });
 });
 
 //start server
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 });
